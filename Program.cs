fa500a44e40a82d4ed56ef8bc763f03182524c8d
Nagaraj Malashetty <nmalashetty@woolworths.com.au>
2025-07-25 15:31:45 +1000

    Add project.assets.json and project.nuget.cache for .NET 9.0 setup
    - Created project.assets.json to define project dependencies and settings for .NET 9.0.
    - Added project.nuget.cache to store NuGet package cache information for the project.

---
+++ Program.cs
@@ -0,0 +1,12 @@
+using System;
+namespace CSharpLearning;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("Hello CSharp");
+        Console.WriteLine("This is a test program.");
+
+    }
+}
